20a6455d7eb9323325331ab5c4aeaabf14a4bbfa
Dmyis <1514378456@qq.com>
2020-01-31 23:47:49 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 const Home = () => import('views/home/Home')
-const Category = () => import('views/category/Category')
-const Cart = () => import('views/cart/Cart')
-const Profile = () => import('views/profile/Profile')
+const Category = () => import('views/category/category')
+const Cart = () => import('views/cart/cart')
+const Profile = () => import('views/profile/profile')
 
 // 1.安装插件
 Vue.use(VueRouter)
